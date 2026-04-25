daef3385c8785ea39dfd57d44ce1ca6b28b0992d
Gaurav <gauravdesale15@gmail.com>
2026-04-25 10:50:32 +0530

    Connected frontend to Render backend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -22,7 +22,7 @@ function Login() {
 
     try {
       const res = await axios.post(
-        `${process.env.REACT_APP_API}/api/auth/login`,
+          "https://chattingo-backend.onrender.com/api/auth/login",
         form
       );
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -25,7 +25,7 @@ function Register() {
 
     try {
       const res = await axios.post(
-        `${process.env.REACT_APP_API}/api/auth/register`,
+        "https://chattingo-backend.onrender.com/api/auth/register",
         form
       );
 
